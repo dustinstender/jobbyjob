93bfda3ee63fad4038ac22fc7821ab63d472aaaa
dustinstender <dustinpstender@gmail.com>
2025-01-02 14:56:24 -0800

    is this my github now?

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>let's just see if this is my github now</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
