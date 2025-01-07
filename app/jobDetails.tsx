d09af8f73b005f38f67ce1ef8b4081211b2d958f
dustinstender <dustinpstender@gmail.com>
2025-01-07 11:12:10 -0800

    improve job details page

--- app/jobDetails.tsx
+++ app/jobDetails.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React from 'react';
 import { Job } from './jobs';
 import { Logo } from './logo';
 
@@ -8,12 +8,18 @@ interface JobDetailsProps {
 
 export const JobDetails: React.FC<JobDetailsProps> = ({job}) => {
 
-    console.log({job})
     return (
-        <div className="bg-gray-800 shadow-lg rounded-lg" style={{padding: 10}}>
+        <div className="bg-gray-800 shadow-lg rounded-lg" style={{padding: 10, height: "80vh"}}>
+            <div style={{display: "flex", justifyContent: "flex-start", alignItems: "center"}}>
             <div className="w-12 h-12 rounded-full bg-gray-700 flex items-center justify-center overflow-hidden mr-4">
                 <Logo name={job.name}  logoUrl={job.logoUrl}/>
             </div>
+            <h1 className="text-3xl font-semibold text-gray-300">{job.name}</h1>
+            </div>
+           <div style={{display: "flex", flexDirection: "column", paddingTop: 20}}>
+           <h3 className="text-2xl font-semibold text-gray-300 mb-6">Position: {job.name}</h3>
+           <p>Description: {job.description}</p>
+           </div>
         </div>
     );
     
